9931e9eeff43983d6fd20dbf2f0b19d3cf5bd76b
Maria Bohorquez <mariaalejandrabm0703@gmail.com>
2020-09-01 20:31:59 -0500

    carpeta docs

---
+++ docs/precache-manifest.aeefaef976dc1f02e3db392c738fdf42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e32f1fc6c0c68c5252a5ea633f119b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e18fe284ff2d70b8163",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e18fe284ff2d70b8163",
+    "url": "/static/js/main.1e751d4d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
